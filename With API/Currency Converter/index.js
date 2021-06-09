8be8314ff993f89f95e480c94689f5104ada61fe
Bernz322 <bernadas_jeffrey@yahoo.com>
2021-06-09 21:45:55 +0800

    first commit

---
+++ With API/Currency Converter/index.js
@@ -0,0 +1,43 @@
+const swapBtn = document.getElementById("btn");
+const currencyOne = document.getElementById("currency-one");
+const currencyTwo = document.getElementById("currency-two");
+const valueOne = document.getElementById("value-one");
+const valueTwo = document.getElementById("value-two");
+const rate = document.getElementById("rate");
+const date = document.getElementById("date");
+
+// Fetching exchange rate
+async function convert() {
+  let currency_one = currencyOne.value;
+  let currency_two = currencyTwo.value;
+  let value_one = valueOne.value;
+
+  const response = await fetch(
+    `https://api.exchangerate.host/convert?from=${currency_one}&to=${currency_two}&amount=${value_one}`
+  );
+  const data = await response.json();
+
+  // returns date
+  date.textContent = data.date;
+
+  // returns the conversion value
+  const exchangeVal = data.info.rate;
+
+  // updates the rate exchange
+  rate.textContent = `1 ${currency_one} = ${exchangeVal} ${currency_two}`;
+
+  // conversion of the inputted currency value
+  valueTwo.value = (valueOne.value * exchangeVal).toFixed(2);
+}
+currencyOne.addEventListener("change", convert);
+currencyTwo.addEventListener("change", convert);
+valueOne.addEventListener("input", convert);
+valueTwo.addEventListener("input", convert);
+
+swapBtn.addEventListener("click", () => {
+  const temp = currencyOne.value;
+  currencyOne.value = currencyTwo.value;
+  currencyTwo.value = temp;
+  convert();
+});
+convert();
---
+++ With API/Currency Converter/readme.md
@@ -0,0 +1,31 @@
+# Currency Converter
+
+## About
+
+This is a currency converter web that uses the exchangerate.host API. Several currency are available for conversion including Bitcoin (BTC). It shows the conversion rate value of two currencies and has a feature of swapping the two currencies from one another.
+
+## Learnings
+
+- Understanding Documentations
+- Fetching APIs
+- Understanding JSON data
+- HTTP Errors/ Response Status Codes
+- Javascript Async/ Await for fetching API
+- Responsive Web and mobile friendly
+
+## Future improvements
+
+- Convert Javascript code to jQuery
+- Add more currencies
+- Improve front-end layout
+
+## Features
+
+- Converts one currency to another.
+- Supports several currencies including Bitcoin (BTC).
+- Daily currency update.
+- Swap currencies from one another.
+
+### Deployment
+
+https://bernz322.github.io/jsproject-currency-converter/
